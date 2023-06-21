7b09a62094a885abed2f7bc8d6dabd2439c3be1b
An7112 <an.nguyent@gotecq.com>
2023-06-21 13:28:21 +0700

    update: dropdown user

--- js/dropdownHover.js
+++ js/dropdownHover.js
@@ -1,18 +1,22 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var items = document.querySelectorAll(".header__item");
-  
-    items.forEach(function(item) {
-      item.addEventListener("mouseover", function() {
-        var listId = this.getAttribute("data-list");
-        var list = document.getElementById(listId);
-        list.classList.add("active");
-      });
-  
-      item.addEventListener("mouseout", function() {
-        var listId = this.getAttribute("data-list");
-        var list = document.getElementById(listId);
-        list.classList.remove("active");
-      });
+document.addEventListener("DOMContentLoaded", function () {
+  var items = document.querySelectorAll(".header__item");
+  var user = document.querySelector(".fullname")
+
+  var elementArray = Array.from(items)
+  elementArray.push(user)
+
+  elementArray.forEach(function (item) {
+    item.addEventListener("mouseover", function () {
+      var listId = this.getAttribute("data-list");
+      var list = document.getElementById(listId);
+      list.classList.add("active");
+      console.log('hover')
+    });
+
+    item.addEventListener("mouseout", function () {
+      var listId = this.getAttribute("data-list");
+      var list = document.getElementById(listId);
+      list.classList.remove("active");
     });
   });
-  
+});
